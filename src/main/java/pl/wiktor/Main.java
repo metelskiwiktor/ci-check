8ca3c6ac431b1de32b73ef6699493fd5f1ad1590
metelskiwiktor <metelski.wiktor@gmail.com>
2025-04-30 22:00:39 +0200

    pwd+chestykel

--- src/main/java/pl/wiktor/Main.java
+++ src/main/java/pl/wiktor/Main.java
@@ -1,7 +1,20 @@
 package pl.wiktor;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(  "Hello world!"  );
+        String a = "as";
+
+        if (a.equals("as")  ) {
+
+
+                 System.out.println("as");
+        } else {
+System.out.println("not as");
+        }
+
+        String e;
     }
 }
